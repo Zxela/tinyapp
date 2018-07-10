2956bac298257deffb29bdb7e2dd0678b4b78214
Your Name <alex.zvaniga@gmail.com>
2018-07-10 18:06:26 -0400

    added response on not found

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -26,21 +26,29 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-    let templateVars = { 
+    let templateVars = {
         shortURL: req.params.id,
-        urls: urlDatabase };
+        urls: urlDatabase
+    };
     res.render("urls_show", templateVars);
-  });
+});
 app.get("/u/:shortURL", (req, res) => {
-    let longURL = urlDatabase[req.params.shortURL];// redirect short URL to Long URL
-    res.redirect(longURL);
+    let longURL = urlDatabase[req.params.shortURL];
+    if (longURL == undefined) {
+        res.status(404)
+            .send("Not found!")// redirect short URL to Long URL
+    }
+    else {
+        res.redirect(longURL);
+    }
 });
 
 app.post("/urls", (req, res) => {
     console.log(req.body);  // debug statement to see POST parameters
     let shortenedURL = generateRandomString()
     urlDatabase[shortenedURL] = req.body.longURL
-    
+
+
     res.redirect(`http://localhost${PORT}/urls/${shortenedURL}`);         // Respond with 'Ok' (we will replace this)
 
 });
@@ -49,5 +57,5 @@ app.listen(PORT, () => {
 });
 // generate string function
 function generateRandomString() {
-    return Math.random().toString(36).substring(2,8);
+    return Math.random().toString(36).substring(2, 8);
 }
