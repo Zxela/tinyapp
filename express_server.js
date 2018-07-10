4ee314f12950bce397231639312297f9d12d6f8c
Your Name <alex.zvaniga@gmail.com>
2018-07-10 14:26:14 -0400

    shows site and shortened url

--- express_server.js
+++ express_server.js
@@ -2,14 +2,23 @@ var express = require("express");
 var app = express();
 var PORT = 8080;
 
-var urlDatabase = {};
+var urlDatabase = {
+    "b2xVn2": "http://www.lighthouselabs.ca",
+    "9sm5xK": "http://www.google.com"
+};
 
+//set view engine
 app.set("view engine", "ejs");
 
 app.get("/urls", (req, res) => {
     let templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:id", (req, res) => {
+    let templateVars = { shortURL: req.params.id };
+    res.render("urls_show", templateVars);
+  });
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`)
